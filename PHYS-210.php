45e284efa92045a3e9265003e3f641640b6a4357
zemrude <zemrude@gmail.com>
2018-02-28 14:25:44 +0100

    Added PHYS-210

---
+++ PHYS-210.php
@@ -0,0 +1,159 @@
+<?php include("header.html")?>
+
+    <div class="row">
+
+
+<h2 id="course-description">Course Description</h2>
+<ul class="vcard">
+  <li> Titulaires: <a href="mailto:juaguila@ulb.ac.be">J. A. Aguilar</a>, <a href="mailto:Ioana.Maris@ulb.ac.be">Ioana Maris</a>,</li>
+  <li> Assistants: <a href="mailto:isabelle.ansseau@ulb.ac.be">Isabelle Ansseau</a>, <a href="mailto:Stephane.Clemmen@ulb.ac.be">Stephane Clemmen </a>, <a href="mailto:lmoureau@ulb.ac.be">Louis Moureaux</a>, <a href="mailto:Jerome.Vandecasteele@ulb.ac.be">Jerome Vandecasteele</a></li>
+  <li>Techniciens: <a href="mailto:akarusho@ulb.ac.be">Anastase Karusho</a></li>
+</ul>
+
+
+<p>The lab is dividided in the following experiments:</p>
+
+<dl class="accordion" data-accordion>
+  <!-- Video -->
+  <dd class="accordion-navigation">
+    <a href="#panel1b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Etudes cin&eacute;matique par video</h5></a>
+    <div id="panel1b" class="content">
+      <ul class="inline-list"> <li><b>Material:</b></li>
+        <li><a href="PHYS-F-210/Tracker.html">Notes (html)</a></li>
+        <li><a href="PHYS-F-210/Tracker.pdf">Notes (pdf)</a></li>
+        <li><a href="http://physlets.org/tracker/">Tracker Software</a></li>
+      </ul>
+    </div>
+  </dd>
+  <!-- Optique -->
+  <dd class="accordion-navigation">
+    <a href="#panel2b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Optique</h5></a>
+    <div id="panel2b" class="content">Eléments d\'optique géométrique. Polarisation de la lumière, milieux anisotropes.
+      <ul class="inline-list">
+        <li><b>Material:</b></li>
+        <li><a href="PHYS-F-210/Optique.html" target="_blank">Notes (html)</a></li>
+        <li><a href="PHYS-F-210/Optique.pdf" target="_blank">Notes (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+
+
+  <!-- Son -->
+  <dd class="accordion-navigation">
+    <a href="#panel1a"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Propagation du son</h5></a>
+    <div id="panel1a" class="content"> Ondes sonores (propagation dans un tube, ondes stationnaires, vitesse du son)
+      <ul class="inline-list">
+        <li><b>Material:</b></li>
+        <li><a href="PHYS-F-210/Son.html" target="_blank">Notes (html)</a></li>
+        <li><a href="PHYS-F-210/Son.pdf" target="_blank">Notes (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+
+  <!--Fluides -->
+  <dd class="accordion-navigation">
+    <a href="#panel3b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Mécanique des fluides</h5></a>
+    <div id="panel3b" class="content">Force de résistance visqueuse et ecoulement visqueux
+      <ul class="inline-list">
+        <li><b>Material:</b></li>
+        <li><a href="PHYS-F-210/viscosite.pdf" target="_blank">Notes Fluides I (pdf)</a></li>
+        <li><a href="PHYS-F-210/Stokes.pdf" target="_blank">Notes Fluides II (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+
+  <!-- Geiger -->
+  <dd class="accordion-navigation">
+    <a href="#panel4b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Compteur Geiger et Désintégrations aléatoires</h5></a>
+    <div id="panel4b" class="content">Introduction à la notion de phénomènes aléatoires- éléments de statistique.
+			 <ul class="inline-list">
+				  <li><b>Material:</b></li>
+				  <li><a href="PHYS-F-210/Compteur Geiger.html" target="_blank">Notes (html)</a></li>
+          <li><a href="PHYS-F-210/Compteur Geiger.pdf" target="_blank">Notes (pdf)</a></li>
+       </ul>
+    </div>
+  </dd>
+
+  <!-- Frank Herz -->
+  <dd class="accordion-navigation">
+    <a href="#panel2a"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Expérience de Frank et Hertz</h5></a>
+    <div id="panel2a" class="content">Quantification des transitions du Mercure
+      <ul class="inline-list">
+            <li><b>Material:</b></li>
+            <li><a href="PHYS-F-210/Frankhertz_2011_rev2016.pdf" target="_blank">Notes (pdf)</a></li>
+            <li><a href="PHYS-F-210/FH_student.pdf" target="_blank">LabView manual (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+
+  <!-- Photoelectric effect -->
+  <dd class="accordion-navigation">
+    <a href="#panel5b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Effet Photoélectrique</h5></a>
+    <div id="panel5b" class="content">Quantification de la lumière
+      <ul class="inline-list">
+            <li><b>Material:</b></li>
+            <li><a href="PHYS-F-210/Notes_EffetPhotoelectrique.pdf" target="_blank">Notes (pdf)</a></li>
+            <li><a href="PHYS-F-210/2002-Photo-E-FusionNewsWinter01.pdf" target="_blank">Fusion article (pdf)</a></li>
+            <li><a href="PHYS-F-210/Millikan_PhysRev-7-355.pdf" target="_blank">Millikan's article (pdf)</a></li>
+            <li><a href="PHYS-F-210/Keesing-EurJPhys2-0143-0807_2_3_003.pdf" target="_blank">Keesing's article (pdf)</a></li>
+            <li><a href="PHYS-F-210/PE_student.pdf" target="_blank">LabView manual (pdf)</a></li>
+      </ul>
+
+    </div>
+  </dd>
+
+  <!--Conductance Quantique-->
+  <dd class="accordion-navigation">
+    <a href="#panel3a"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Conductance quantique</h5></a>
+    <div id="panel3a" class="content">
+      <ul class="inline-list">
+            <li><b>Material:</b></li>
+            <li><a href="PHYS-F-210/CondQuant_v4.pdf" target="_blank">Notes (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+
+  <!--Coax -->
+  <dd class="accordion-navigation">
+    <a href="#panel6b"><span><i class="fa fa-plus fa-lg fa-fw"></i><i class="fa fa-minus fa-lg"></i></span><h5>Cables coaxiaux</h5></a>
+    <div id="panel6b" class="content"> Propagation dans un câble coaxial, ondes stationnaires
+      <ul class="inline-list">
+        <li><b>Material:</b></li>
+        <li><a href="PHYS-F-210/Coaxe.html">Notes (html)</a></li>
+        <li><a href="PHYS-F-210/Coaxe.pdf">Notes (pdf)</a></li>
+        <li><a href="PHYS-F-210/Annex1.pdf">Annexe 1 (pdf)</a></li>
+        <li><a href="PHYS-F-210/Annex2.pdf">Annexe 2 (pdf)</a></li>
+        <li><a href="PHYS-F-210/Annex3.pdf">Annexe 3 (pdf)</a></li>
+      </ul>
+    </div>
+  </dd>
+ </dl>
+
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script type="text/javascript" src="slick/slick.min.js"></script>
+
+<script>
+ $(document).foundation();
+</script>
+
+<script>
+  $(".accordion dd").on("click", "a:eq(0)", function (event)
+      {
+        var dd_parent = $(this).parent();
+
+        if(dd_parent.hasClass('active'))
+          $(".accordion dd div.content:visible").slideToggle("normal");
+        else
+        {
+          $(".accordion dd div.content:visible").slideToggle("normal");
+          $(this).parent().find(".content").slideToggle("normal");
+        }
+   });
+</script>
+
+
+
+<?php include("footer.html")?>
